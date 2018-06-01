09f807c65fbaa591b90d32c964b1bcdc435f03c3
prolin99 <prolin99@gmail.com>
2018-06-01 09:40:29 +0800

    註解

--- 2-1.py
+++ 2-1.py
@@ -1,17 +1,46 @@
+#陣列
 list1=['aaa','bbb','ccc']
+list1[2]='change'
+#合成一字串
 s=','.join(list1)
 print(s)
-t=1,2,3
-t=(1,2,3)
-print(t)
-print(t[2])
-a,b,c=t
+
+
+#tuple 和陣列類似，但無法再修改
+t1=1,2,3
+print(t1)
+
+t2=(1,2,3)
+#無法做修改  如：  t2[2]=5
+print(t2)
+print(t2[2])
+
+#分別取值
+x,y,z=list1
+print(x,y,z)
+
+
+a,b,c=t2
 print(a,b,c)
+
+#互換
+x,y = y,x
+print(x,y,z)
+
 a,b=b,a
 print(a,b,c)
-t2=t,4,5
+
+# tuple 增加元素
+t2=t1,4,5
 print(t2)
 print(t2[0])
+
+t3=t1 + (4,5)
+print(t3)
+print(t3[0])
+
+
+
 t3=('x')
 t4=('x',)
 
--- for.py
+++ for.py
@@ -1,12 +1,18 @@
 list1=range(0,5)
 print(list1)
+#放入陣列
 t = [n for n in range(0,5)]
 print (t)
 
+#字串也可以視為陣列
 list3='python'
 for t in list3:
     print(t)
 
+#字串分割
 list3= "2016/12/31".split('/')
-print(list3).
+
+print(list3)
+
+#陣列內容 反轉 [開始：結束：移動數]
 print(list3[::-1])
